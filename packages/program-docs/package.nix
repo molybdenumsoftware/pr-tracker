28e616d18d1f88e8c64025aebc88735e7f1aeeff
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-03-08 15:12:48 +0700

    refactor(flake): rename documentation package

--- packages/program-docs/package.nix
+++ packages/program-docs/package.nix
@@ -7,7 +7,7 @@
 cargoDoc {
   inherit src cargoArtifacts GITHUB_GRAPHQL_SCHEMA;
 
-  pname = "pr-tracker-docs";
+  pname = "pr-tracker-program-docs";
   version = "unversioned";
 
   cargoDocExtraArgs = "--package pr-tracker-fetcher-config --package pr-tracker-api-config --no-deps";
