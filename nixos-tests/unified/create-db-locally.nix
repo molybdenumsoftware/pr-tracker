0c26d73763bc17ac6d25a70a23ef392aee11b8dd
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-03-25 14:52:38 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:nixos-tests/unified/create-db-locally.nix

--- nixos-tests/unified/create-db-locally.nix
+++ nixos-tests/unified/create-db-locally.nix
@@ -17,12 +17,22 @@ in {
 
   nixpkgs.hostPlatform = system;
 
+  # shared
   services.pr-tracker-unified.enable = true;
-  systemd.services.pr-tracker-api.environment.RUST_BACKTRACE = "1";
-  systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
-  services.pr-tracker-unified.port = port;
-  services.pr-tracker-unified.user = user;
   services.pr-tracker-unified.createDatabaseLocally = true;
+  services.pr-tracker-unified.user = user;
+  services.pr-tracker-unified.group = user;
+
+  # api
+  services.pr-tracker-unified.port = port;
+  systemd.services.pr-tracker-api.environment.RUST_BACKTRACE = "1";
+
+  # fetcher
   services.pr-tracker-unified.branchPatterns = ["*"];
+  services.pr-tracker-unified.githubApiTokenFile = writeText "gh-auth-token" "hunter2";
+  services.pr-tracker-unified.fetchOnCalendar = "*:*:*"; # every single second
+  services.pr-tracker-unified.repo.owner = "molybdenumsoftware";
+  services.pr-tracker-unified.repo.name = "pr-tracker";
+  systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
 }
 
