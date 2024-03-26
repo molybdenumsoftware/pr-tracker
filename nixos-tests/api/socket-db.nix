50a5bc4affeec1801cbe5da55049817352b41aee
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-26 18:14:45 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:nixos-tests/fetcher/socket-db.nix

--- nixos-tests/api/socket-db.nix
+++ nixos-tests/api/socket-db.nix
@@ -6,11 +6,8 @@ pr-tracker: {
   inherit
     (pkgs)
     system
-    writeText
     ;
   port = 7000;
-  pgPort = 5432;
-  user = "pr-tracker";
 in {
   imports = [pr-tracker.nixosModules.api];
 
@@ -20,6 +17,5 @@ in {
   services.pr-tracker.api.package = pr-tracker.packages.${system}.api.overrideAttrs {dontStrip = true;};
   systemd.services.pr-tracker-api.environment.RUST_BACKTRACE = "1";
   services.pr-tracker.api.port = port;
-  services.pr-tracker.api.user = user;
   services.pr-tracker.db.createLocally = true;
 }
--- nixos-tests/fetcher/socket-db.nix
+++ nixos-tests/fetcher/socket-db.nix
@@ -8,31 +8,15 @@ pr-tracker: {
     system
     writeText
     ;
-  pgPort = 5432;
-  user = "pr-tracker";
 in {
   imports = [pr-tracker.nixosModules.fetcher];
 
   nixpkgs.hostPlatform = system;
 
-  services.postgresql.enable = true;
-  services.postgresql.port = pgPort;
-  services.postgresql.ensureDatabases = [user];
-  services.postgresql.ensureUsers = [
-    {
-      name = user;
-      ensureDBOwnership = true;
-    }
-  ];
-
   services.pr-tracker.fetcher.enable = true;
   services.pr-tracker.fetcher.package = pr-tracker.packages.${system}.fetcher.overrideAttrs {dontStrip = true;};
   systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
-  services.pr-tracker.fetcher.user = user;
-  services.pr-tracker.db.urlParams.host = "/run/postgresql";
-  services.pr-tracker.db.urlParams.port = toString pgPort;
-  services.pr-tracker.db.urlParams.dbname = user;
-  services.pr-tracker.db.isLocal = true;
+  services.pr-tracker.db.createLocally = true;
   services.pr-tracker.fetcher.onCalendar = "*:*:*"; # every single second
   services.pr-tracker.fetcher.githubApiTokenFile = writeText "gh-auth-token" "hunter2";
   services.pr-tracker.fetcher.branchPatterns = ["*"];
