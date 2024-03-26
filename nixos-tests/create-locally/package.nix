8030d3e1b642fe953e8b9fa6f8addb5291931ffc
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-03-26 14:27:30 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:nixos-tests/create-locally/package.nix

--- nixos-tests/create-locally/package.nix
+++ nixos-tests/create-locally/package.nix
@@ -8,20 +8,26 @@ pr-tracker: {
     system
     writeText
     ;
-  pgPort = 5432;
+  port = 7000;
   user = "pr-tracker";
 in {
-  imports = [pr-tracker.nixosModules.fetcher];
+  imports = [pr-tracker.nixosModules.api];
 
   nixpkgs.hostPlatform = system;
 
-  services.pr-tracker.fetcher.enable = true;
-  systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
-  services.pr-tracker.fetcher.user = user;
   services.pr-tracker.db = "createLocally";
+
+  services.pr-tracker.api.enable = true;
+  services.pr-tracker.api.port = port;
+  services.pr-tracker.api.user = user;
+  systemd.services.pr-tracker-api.environment.RUST_BACKTRACE = "1";
+
+  services.pr-tracker.fetcher.enable = true;
+  services.pr-tracker.fetcher.user = user;
   services.pr-tracker.fetcher.onCalendar = "*:*:*"; # every single second
   services.pr-tracker.fetcher.githubApiTokenFile = writeText "gh-auth-token" "hunter2";
   services.pr-tracker.fetcher.branchPatterns = ["*"];
   services.pr-tracker.fetcher.repo.owner = "molybdenumsoftware";
   services.pr-tracker.fetcher.repo.name = "pr-tracker";
+  systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
 }
