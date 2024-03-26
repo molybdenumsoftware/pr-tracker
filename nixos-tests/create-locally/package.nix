9eb8fd50431eb4d9aaf8722993c3d77f4fb030a5
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-26 18:29:25 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:nixos-tests/create-locally/package.nix

--- nixos-tests/create-locally/package.nix
+++ nixos-tests/create-locally/package.nix
@@ -2,49 +2,54 @@
   pr-tracker,
   lib,
   nixosTest,
-}:
-nixosTest {
-  name = "create-locally";
+}: let
+  inherit (builtins) tryEval;
 
-  nodes.pr_tracker = {
-    pkgs,
-    config,
-    ...
-  }: let
-    inherit
-      (pkgs)
-      system
-      writeText
-      ;
-    port = 7000;
-  in {
-    imports = [
-      pr-tracker.nixosModules.api
-      pr-tracker.nixosModules.fetcher
-    ];
+  port = 7000;
+in
+  assert tryEval;
+    nixosTest {
+      name = "create-locally";
 
-    nixpkgs.hostPlatform = system;
+      nodes.pr_tracker = {
+        pkgs,
+        config,
+        ...
+      }: let
+        inherit
+          (pkgs)
+          system
+          writeText
+          ;
+      in {
+        imports = [
+          pr-tracker.nixosModules.api
+          pr-tracker.nixosModules.fetcher
+        ];
 
-    services.pr-tracker.db.createLocally = true;
+        nixpkgs.hostPlatform = system;
 
-    services.pr-tracker.api.enable = true;
-    services.pr-tracker.api.port = port;
-    services.pr-tracker.api.package = pr-tracker.packages.${system}.api.overrideAttrs {dontStrip = true;};
-    systemd.services.pr-tracker-api.environment.RUST_BACKTRACE = "1";
+        services.pr-tracker.db.createLocally = true;
 
-    services.pr-tracker.fetcher.enable = true;
-    services.pr-tracker.fetcher.onCalendar = "*:*:*"; # every single second
-    services.pr-tracker.fetcher.githubApiTokenFile = writeText "gh-auth-token" "hunter2";
-    services.pr-tracker.fetcher.branchPatterns = ["*"];
-    services.pr-tracker.fetcher.repo.owner = "molybdenumsoftware";
-    services.pr-tracker.fetcher.repo.name = "pr-tracker";
-    services.pr-tracker.fetcher.package = pr-tracker.packages.${system}.fetcher.overrideAttrs {dontStrip = true;};
-    systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
-  };
+        services.pr-tracker.api.enable = true;
+        services.pr-tracker.api.port = port;
+        services.pr-tracker.api.package = pr-tracker.packages.${system}.api.overrideAttrs {dontStrip = true;};
+        systemd.services.pr-tracker-api.environment.RUST_BACKTRACE = "1";
 
-  testScript = ''
-    pr_tracker.wait_for_unit("pr-tracker-api.service")
-    pr_tracker.succeed("curl --fail http://localhost:${port}/api/v1/healthcheck")
-    pr_tracker.wait_until_succeeds("journalctl -u pr-tracker-fetcher.service --grep 'error sending request for url'", timeout=60)
-  '';
-}
+        services.pr-tracker.fetcher.enable = true;
+        services.pr-tracker.fetcher.onCalendar = "*:*:*"; # every single second
+        services.pr-tracker.fetcher.githubApiTokenFile = writeText "gh-auth-token" "hunter2";
+        services.pr-tracker.fetcher.branchPatterns = ["*"];
+        services.pr-tracker.fetcher.repo.owner = "molybdenumsoftware";
+        services.pr-tracker.fetcher.repo.name = "pr-tracker";
+        services.pr-tracker.fetcher.package = pr-tracker.packages.${system}.fetcher.overrideAttrs {dontStrip = true;};
+        systemd.services.pr-tracker-fetcher.environment.RUST_BACKTRACE = "1";
+      };
+
+      testScript = ''
+        pr_tracker.wait_for_unit("pr-tracker-api.service")
+        pr_tracker.succeed("curl --fail http://localhost:${port}/api/v1/healthcheck")
+        pr_tracker.wait_until_succeeds("journalctl -u pr-tracker-fetcher.service --grep 'error sending request for url'", timeout=60)
+      '';
+    }
+
