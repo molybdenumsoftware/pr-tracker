46aa05f4e828806d8f574173d117403389c37a2f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-08-25 18:26:47 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/api/tests/openapi.rs

--- crates/api/tests/openapi.rs
+++ crates/api/tests/openapi.rs
@@ -5,13 +5,22 @@ use pr_tracker_api::ApiDoc;
 use rocket::futures::FutureExt;
 use test_util::TestContext;
 
+#[derive(serde::Deserialize)]
+struct Task {
+    id: usize,
+    complete: bool,
+    text: String,
+}
+
 #[tokio::test]
 async fn openapi() {
     TestContext::with(|ctx| {
         async {
             let response = ctx.client.get("/openapi.json").dispatch().await;
             assert_eq!(response.status(), rocket::http::Status::Ok);
-            let response: ApiDoc = response.into_json().await.unwrap();
+            let body = response.body();
+            let body = body.to_string().await.unwrap();
+            response.into_json::<Task>().await.unwrap();
         }
         .boxed()
     })
