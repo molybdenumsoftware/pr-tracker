e2f6c4ff80bf017e3e7d36e4175f1b1aeeb91b94
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-11-05 00:18:39 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/api/tests/openapi.rs

--- crates/api/tests/openapi.rs
+++ crates/api/tests/openapi.rs
@@ -6,7 +6,7 @@ use futures::FutureExt;
 use test_util::TestContext;
 
 test![json, |ctx: TestContext| async move {
-    let response = ctx.client().get("docs/openapi.json").send().await;
+    let response = ctx.client().get("/api/v1/openapi.json").send().await;
     response.assert_status_is_ok();
     let json = response.json().await;
     dbg!(json);
