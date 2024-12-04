3b573cdc268950c60a0a2494c38584c8b9b18021
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-04 13:31:35 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/program-docs.nix

--- modules/program-docs.nix
+++ modules/program-docs.nix
@@ -1,31 +1,9 @@
 {
-  self,
-  lib,
-  GITHUB_GRAPHQL_SCHEMA,
-  ...
-}: {
   perSystem = {
     config,
-    pkgs,
     ...
   }: {
     packages.program-docs = config.nci.outputs.default.docs;
     checks.program-docs = config.packages.program-docs;
-    # <<< packages.program-docs = config.nci.lib.buildCrate {
-    # <<<   src = lib.traceValSeqN 1 (lib.attrNames config.nci.outputs.default) config.nci.projects.default.path;
-    # <<<   drvConfig = {
-    # <<<     env = {inherit GITHUB_GRAPHQL_SCHEMA;};
-    # <<<     rust-crane = {
-    # <<<       buildCommand = "doc";
-    # <<<       buildFlags = [
-    # <<<         "--package"
-    # <<<         "pr-tracker-fetcher-config"
-    # <<<         "--package"
-    # <<<         "pr-tracker-api-config"
-    # <<<         "--no-deps"
-    # <<<       ];
-    # <<<     };
-    # <<<   };
-    # <<< };
   };
 }
