1a6b6fdeca5b23201ee509161fa02e3aa03a737b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-06 12:35:06 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/api/default.nix

--- modules/api/default.nix
+++ modules/api/default.nix
@@ -7,14 +7,7 @@
     pkgs,
     ...
   }: {
-    nci.crates.pr-tracker-api = {
-      drvConfig = {
-        mkDerivation = {
-          nativeCheckInputs = [pkgs.postgresql];
-        };
-      };
-    };
-
+    nci.crates.pr-tracker-api.drvConfig.mkDerivation.nativeCheckInputs = [pkgs.postgresql];
     packages.api = config.nci.outputs.pr-tracker-api.packages.release;
     checks."packages/api" = self'.packages.api;
   };
