ddbca65d678c37f628087653830475e89cec65ab
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-12-10 23:35:54 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/program-docs.nix

--- modules/fetcher/default.nix
+++ modules/fetcher/default.nix
@@ -14,11 +14,14 @@
     lib,
     ...
   }: {
-    nci.crates.pr-tracker-fetcher.drvConfig.env = {
-      inherit GITHUB_GRAPHQL_SCHEMA;
-      POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
-      POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
-      GIT = lib.getExe pkgs.git;
+    nci.crates.pr-tracker-fetcher.drvConfig = {
+      mkDerivation.meta.mainProgram = "pr-tracker-fetcher";
+      env = {
+        inherit GITHUB_GRAPHQL_SCHEMA;
+        POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
+        POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
+        GIT = lib.getExe pkgs.git;
+      };
     };
 
     packages.fetcher = config.nci.outputs.pr-tracker-fetcher.packages.release;
--- modules/program-docs.nix
+++ modules/program-docs.nix
@@ -1,6 +1,16 @@
 {
-  perSystem = {config, ...}: {
+  perSystem = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: {
     packages.program-docs = config.nci.outputs.default.docs;
+    nci.crates.docs.drvConfig.env = {
+      POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
+      POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
+      GIT = lib.getExe pkgs.git;
+    };
     checks.program-docs = config.packages.program-docs;
   };
 }
