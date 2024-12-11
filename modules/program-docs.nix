9ebaec9cc16843c1279e6729d62e8844f8234dca
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-12-10 23:55:18 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/program-docs.nix

--- modules/program-docs.nix
+++ modules/program-docs.nix
@@ -5,7 +5,8 @@
     pkgs,
     ...
   }: {
-    packages.program-docs = lib.traceSeqN 2 config.nci.crates config.nci.outputs.default.docs;
+    packages.program-docs = config.nci.outputs.default.docs;
+    # <<< packages.program-docs = lib.traceSeqN 2 config.nci.crates config.nci.outputs.default.docs;
     # nci.crates.docs.drvConfig.env = {
     #   POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
     #   POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
