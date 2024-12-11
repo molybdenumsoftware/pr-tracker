cc50fd9184185283ba55ad9332d7cc249fed0382
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-12-11 00:10:42 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/program-docs.nix

--- modules/program-docs.nix
+++ modules/program-docs.nix
@@ -5,6 +5,15 @@
     pkgs,
     ...
   }: {
+
+    #<<< TODO: extract >>>
+    nci.crates.db-context.drvConfig.mkDerivation.env = {
+      POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
+      POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
+      GIT = lib.getExe pkgs.git;
+    };
+    #<<< TODO: extract >>>
+
     packages.program-docs = config.nci.outputs.default.docs.overrideAttrs {
       env = {
         POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
