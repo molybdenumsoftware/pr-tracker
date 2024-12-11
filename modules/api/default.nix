30e011a332e4a5bf52bd875b6f1a68fdb4926ecf
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-11 14:26:39 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/api/default.nix

--- modules/api/default.nix
+++ modules/api/default.nix
@@ -8,7 +8,7 @@
     pkgs,
     ...
   }: {
-    nci.crates.pr-tracker-api.drvConfig.mkDerivation.nativeCheckInputs = [pkgs.postgresql];
+    nci.crates.pr-tracker-api.drvConfig.mkDerivation.meta.mainProgram = "pr-tracker-api";
     packages.api = config.nci.outputs.pr-tracker-api.packages.release;
     nci.crates.pr-tracker-api.drvConfig.env = {
       POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
