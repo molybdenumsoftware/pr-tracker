d37fa677eaf7ddea4cd6ad434a9063daab4471e3
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-11 14:42:41 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/program-docs.nix

--- modules/program-docs.nix
+++ modules/program-docs.nix
@@ -5,12 +5,12 @@
     pkgs,
     ...
   }: {
-    packages.program-docs = config.nci.outputs.default.docs;
-    nci.crates.docs.drvConfig.env = {
-      POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
-      POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
-      GIT = lib.getExe pkgs.git;
-    };
+    packages.program-docs = lib.traceSeqN 2 config.nci.crates config.nci.outputs.default.docs;
+    # nci.crates.docs.drvConfig.env = {
+    #   POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
+    #   POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
+    #   GIT = lib.getExe pkgs.git;
+    # };
     checks.program-docs = config.packages.program-docs;
   };
 }
