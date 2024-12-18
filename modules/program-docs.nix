86d73533a2eae281b9d7cc8226f2e1f7436055c0
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-12-17 22:09:32 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/program-docs.nix

--- modules/program-docs.nix
+++ modules/program-docs.nix
@@ -1,18 +1,9 @@
 {
   perSystem = {
     config,
-    lib,
-    pkgs,
     ...
   }: {
-    # <<< packages.program-docs = config.nci.outputs.default.docs.overrideAttrs {
-    # <<<   env = {
-    # <<<     POSTGRESQL_INITDB = lib.getExe' pkgs.postgresql "initdb";
-    # <<<     POSTGRESQL_POSTGRES = lib.getExe' pkgs.postgresql "postgres";
-    # <<<     GIT = lib.getExe pkgs.git;
-    # <<<   };
-    # <<< };
     packages.program-docs = config.nci.outputs.default.docs;
-    checks.program-docs = config.packages.program-docs;
+    checks."packages/program-docs" = config.packages.program-docs;
   };
 }
