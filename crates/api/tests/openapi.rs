a17ad7a4cc5c810215a2f4ee2845119fb0ea104a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-04-16 13:19:41 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/api/tests/openapi.rs

--- crates/api/tests/openapi.rs
+++ crates/api/tests/openapi.rs
@@ -3,6 +3,7 @@
 mod test_util;
 
 use futures::FutureExt;
+use poem::http::StatusCode;
 use test_util::TestContext;
 
 test![json, |ctx: TestContext| async move {
@@ -12,10 +13,8 @@ test![json, |ctx: TestContext| async move {
     dbg!(json);
 }];
 
-//<<<
-test![json, |ctx: TestContext| async move {
-    let response = ctx.client().get("/openapi.json").send().await;
-    response.assert_status_is_ok();
-    let json = response.json().await;
-    dbg!(json);
+test![homepage_redirect, |ctx: TestContext| async move {
+    let response = ctx.client().get("/").send().await;
+    response.assert_status(StatusCode::SEE_OTHER);
+    assert_eq!()
 }];
