fe51eae59f0d843c75f84da32694b178b631a7cf
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-04-15 23:40:06 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/api/src/lib.rs

--- crates/api/tests/openapi.rs
+++ crates/api/tests/openapi.rs
@@ -9,12 +9,17 @@ use test_util::TestContext;
 test![json, |ctx: TestContext| async move {
     let response = ctx.client().get("/openapi.json").send().await;
     response.assert_status_is_ok();
-    let json = response.json().await;
-    dbg!(json);
+    response.assert_content_type("application/json");
 }];
 
 test![homepage_redirect, |ctx: TestContext| async move {
     let response = ctx.client().get("/").send().await;
     response.assert_status(StatusCode::SEE_OTHER);
-    assert_eq!()
+    response.assert_header("Location", "/api-docs");
+}];
+
+test![api_docs, |ctx: TestContext| async move {
+    let response = ctx.client().get("/api-docs").send().await;
+    response.assert_status_is_ok();
+    response.assert_content_type("text/html; charset=utf-8");
 }];
