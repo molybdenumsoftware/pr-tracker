f010a0c2fbd83bcd5043e2f92dcbfaf644430784
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-21 15:10:56 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/api-config/src/lib.rs

--- crates/api-config/src/lib.rs
+++ crates/api-config/src/lib.rs
@@ -10,19 +10,7 @@ mod environment {
     use super::TracingFilter;
     use confique::Config;
 
-    /// See documentation for each field.
-    #[derive(Debug, Config)]
-    pub struct Environment {
-        #[doc = include_str!("../../DATABASE_URL.md")]
-        #[config(env = "PR_TRACKER_API_DATABASE_URL")]
-        pub PR_TRACKER_API_DATABASE_URL: String,
-        #[config(env = "PR_TRACKER_API_PORT")]
-        #[doc = include_str!("../PORT.md")]
-        pub PR_TRACKER_API_PORT: u16,
-        #[config(env = "PR_TRACKER_TRACING_FILTER")]
-        #[doc = include_str!("../TRACING_FILTER.md")]
-        pub PR_TRACKER_TRACING_FILTER: Option<TracingFilter>,
-    }
+    include!(env!("api_config_snippet"));
 }
 
 #[serde_as]
--- modules/api/default.nix
+++ modules/api/default.nix
@@ -11,7 +11,25 @@
     {
       nci = {
         crates = {
-          pr-tracker-api.drvConfig.mkDerivation.meta.mainProgram = "pr-tracker-api";
+          pr-tracker-api.drvConfig = {
+            env.api_config_snippet =
+              # rust
+              ''
+                #[derive(Debug, Config)]
+                pub struct Environment {
+                  #[doc = include_str!("../../DATABASE_URL.md")]
+                  #[config(env = "PR_TRACKER_API_DATABASE_URL")]
+                  pub PR_TRACKER_API_DATABASE_URL: String,
+                  #[config(env = "PR_TRACKER_API_PORT")]
+                  #[doc = include_str!("../PORT.md")]
+                  pub PR_TRACKER_API_PORT: u16,
+                  #[config(env = "PR_TRACKER_TRACING_FILTER")]
+                  #[doc = include_str!("../TRACING_FILTER.md")]
+                  pub PR_TRACKER_TRACING_FILTER: Option<TracingFilter>,
+                }
+              '';
+            mkDerivation.meta.mainProgram = "pr-tracker-api";
+          };
           pr-tracker-api-config.includeInProjectDocs = true;
         };
         projects.default.fileset = lib.fileset.unions [
