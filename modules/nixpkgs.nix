5badd1cfd8648dba4e43efda36a8d129607bb526
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-11 19:43:38 +0700

    chore: nixos nixpkgs module in perSystem

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,3 +1,8 @@
-{
+{ inputs, ... }: {
   flake-file.inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  perSystem = { system, ... }: {
+    imports = [ "${inputs.nixpkgs}/nixos/modules/misc/nixpkgs.nix" ];
+    nixpkgs = { inherit system; };
+  };
 }
