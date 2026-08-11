abd5dabdb141da6602254fb02d626b0cc833a950
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-11 14:40:42 +0700

    style: sort flake inputs in preparation for flake-file

--- flake.nix
+++ flake.nix
@@ -4,23 +4,13 @@
   };
 
   inputs = {
-    import-tree.url = "github:denful/import-tree";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nci = {
-      url = "github:yusdacra/nix-cargo-integration";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        parts.follows = "flake-parts";
-        treefmt.follows = "treefmt-nix";
-      };
-    };
     devshell = {
       url = "github:numtide/devshell";
       flake = false;
     };
     flake-parts = {
-      inputs.nixpkgs-lib.follows = "nixpkgs";
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
@@ -30,10 +20,20 @@
       url = "github:octokit/graphql-schema";
       flake = false;
     };
+    import-tree.url = "github:denful/import-tree";
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        parts.follows = "flake-parts";
+        treefmt.follows = "treefmt-nix";
+      };
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     treefmt-nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
