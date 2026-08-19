d1161ae24aa3713733051f6a333035cadba8ec75
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-19 10:46:27 +0700

    chore: 2 build vars projects.default -> fetcher

--- modules/fetcher/default.nix
+++ modules/fetcher/default.nix
@@ -84,18 +84,12 @@
         };
       };
 
-      nci = {
-        projects.default = {
-          drvConfig.env = {
-            GITHUB_GRAPHQL_SCHEMA = "${inputs.github-graphql-schema}/schema.graphql";
-            GIT = lib.getExe pkgs.git;
-          };
-        };
-        crates = {
-          pr-tracker-fetcher.drvConfig = {
-            mkDerivation.meta.mainProgram = "pr-tracker-fetcher";
-            env.fetcher_config_snippet = writeEnvironmentStructFile "fetcher" fetcher.environmentVariables;
-          };
+      nci.crates.pr-tracker-fetcher.drvConfig = {
+        mkDerivation.meta.mainProgram = "pr-tracker-fetcher";
+        env = {
+          GITHUB_GRAPHQL_SCHEMA = "${inputs.github-graphql-schema}/schema.graphql";
+          GIT = lib.getExe pkgs.git;
+          fetcher_config_snippet = writeEnvironmentStructFile "fetcher" fetcher.environmentVariables;
         };
       };
 
